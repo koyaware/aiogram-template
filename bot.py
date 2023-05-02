7458d71d2ce3add27bcb76273d5980623e6a998b
koyaware <rundelchaos@gmail.com>
2023-05-02 20:43:44 +0500

    aiogram template

---
+++ bot.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+
+from tgbot.config import load_config
+from tgbot.filters import register_all_filters
+from tgbot.handlers import register_all_handlers
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+    )
+    logger.info("Starting bot")
+    config = load_config(".env")
+
+    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
+    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp = Dispatcher(bot, storage=storage)
+
+    bot['config'] = config
+    register_all_filters(dp)
+    register_all_handlers(dp)
+
+    # start
+    try:
+        await dp.start_polling()
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error("Bot stopped!")
